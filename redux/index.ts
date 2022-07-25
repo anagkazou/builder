e5a4509719fe5a4af128575171af9a05ef6a5368
anagkazou <thedavidomole@gmail.com>
2022-07-25 18:48:14 +0100

    :sparkles: implemented redux persist

--- redux/index.ts
+++ redux/index.ts
@@ -1,12 +1,29 @@
-import { Action, configureStore, Store, ThunkAction } from "@reduxjs/toolkit";
+import {
+  Action,
+  combineReducers,
+  configureStore,
+  Store,
+  ThunkAction,
+} from "@reduxjs/toolkit";
 import { type } from "os";
 
 import userReducer from "./features/auth/authSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user"],
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
 
   middleware(getDefaultMiddleware) {
     return getDefaultMiddleware({
@@ -14,6 +31,8 @@ export const store = configureStore({
     });
   },
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
