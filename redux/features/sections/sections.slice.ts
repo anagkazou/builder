7e53d741775abb126d518494f3c40df3dc6adced
anagkazou <thedavidomole@gmail.com>
2022-09-21 16:57:29 +0100

    :construction: build out links feature flow

--- redux/features/sections/sections.slice.ts
+++ redux/features/sections/sections.slice.ts
@@ -3,35 +3,52 @@ import { RootState } from "../..";
 import { Views } from "../../../pages/dashboard/side-panels/section-panels";
 
 export type TextBox = {
-  title: string;
-  content: string;
-  type: string;
+  title: string; content: string; type: string;
 };
 export type Links = {
-  type: string,
-  links: Array<LinkItem>
+  type: string, links: Array<LinkItem>
 }
 export type LinkItem = {
-  description: string;
-  url: string;
+  description: string; url: string;
 }
 export type Sections = {
   items: Array<TextBox | Links>;
 };
 const initialState: Sections = { items: [] };
+export const DEFAULT_SOCIAL_LINKS = [{
+  network: "Spotify", enabled: false, value: "", prefix: "", isUrl: true
+}, {
+  network: "Facebook", enabled: false, value: "", prefix: "", isUrl: true
+}, {
+  network: "Twitter", enabled: false, value: "", prefix: "", isUrl: false
+}, {
+  network: "Youtube", enabled: false, value: "", prefix: "", isUrl: true
+}, {
+  network: "Snapchat", enabled: false, value: "", prefix: "", isUrl: false
+}, {
+  network: "Linkedin", enabled: false, value: "", prefix: "", isUrl: true
+}, {
+  network: "Twitch", enabled: false, value: "", prefix: "", isUrl: false
+}, {
+  network: "Envelope", enabled: false, isUrl: true, value: "", prefix: ""
+}];
+
+/**todo:
+ *
+ * Get sections from firestore in page bucket
+ * If there is no data there, initialize with
+ * **/
 export const SectionSLice = createSlice({
-  name: "sections",
-  initialState,
-  reducers: {
+  name: "sections", initialState, reducers: {
     setReorderedSectionItems(state, action) {
       state.items = action.payload;
-    },
-    addNewSectionItem: (state, action) => {
+    }, addNewSectionItem: (state, action) => {
       state.items.push(action.payload);
-    },
-    deleteSectionItem: (state, action) => {
+    }, deleteSectionItem: (state, action) => {
       state.items.slice(action.payload, 1);
     },
+
+
     addNewLinkItem: (state, action) => {
       const i = state.items.findIndex((item, index) => item.type == Views.LINKS);
       //  links.push(action.payload);
@@ -42,31 +59,35 @@ export const SectionSLice = createSlice({
         linksObj.links = [...linksObj.links, action.payload];
       } else {
         state.items.push({
-          type: Views.LINKS,
-          links: [action.payload]
+          type: Views.LINKS, links: [action.payload]
         });
       }
     },
-    addNewSocialLink: (state, action) => {
+
+    //SOCIAL - LINKS REDUCERS
+
+    saveSocialLinks: (state, action) => {
       const i = state.items.findIndex((item, index) => item.type == Views.SOCIALS);
       //  links.push(action.payload);
       console.log("LINKS::", i);
       if (i !== -1) {
         let linksObj = state.items[i];
         // @ts-ignore
-        linksObj.links = [...linksObj.links, action.payload];
+        linksObj.links = action.payload;
       } else {
         state.items.push({
-          type: Views.SOCIALS,
-          links: [action.payload]
+          type: Views.SOCIALS, links: action.payload
         });
       }
     }
   }
 });
 
-export const { addNewSectionItem, addNewLinkItem } = SectionSLice.actions;
+export const {
+  addNewSectionItem, addNewLinkItem, saveSocialLinks
+} = SectionSLice.actions;
 
+export const selectSocialLinks = (state: RootState) => state.sections.items.findIndex((item, index) => item.type == Views.SOCIALS);
 export default SectionSLice.reducer;
 
 export const selectSections = (state: RootState) => state.sections;
