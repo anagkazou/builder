c83bd0f0d518ba6e104cd27617ee327ee1e9db86
anagkazou <thedavidomole@gmail.com>
2022-09-21 16:57:29 +0100

    :construction: build out links feature flow

--- pages/dashboard/dashboard-preview/index.tsx
+++ pages/dashboard/dashboard-preview/index.tsx
@@ -2,20 +2,22 @@ import { NextPage } from "next";
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectpage } from "../../../redux/features/page-data/page-data.slice";
-import { selectSections } from "../../../redux/features/sections/sections.slice";
+import {
+  selectSections
+} from "../../../redux/features/sections/sections.slice";
 
 export const DashboardPreview: React.FC = () => {
   const pageState = useSelector(selectpage);
   const sectionState = useSelector((selectSections));
 
-  return (
-    <>
+  return (<>
       <p className="text-black">{pageState?.handle}</p>
 
       <div className=" bg-slate-300 dashboard-preview text-black">
-        {
-          JSON.stringify(sectionState)
-        }
+        <div className=" text-xs w-96 text-red-600 "
+             style={{ width: "100px" }}>
+          {JSON.stringify(sectionState)}
+        </div>
       </div>
     </>
 
--- pages/dashboard/side-panels/section-panels/socials-view.tsx
+++ pages/dashboard/side-panels/section-panels/socials-view.tsx
@@ -19,43 +19,18 @@ import {
   IconDefinition
 } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
-import { faBox, faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faBox, faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { Simulate } from "react-dom/test-utils";
+import {
+  DEFAULT_SOCIAL_LINKS, saveSocialLinks, selectSections, selectSocialLinks
+} from "../../../../redux/features/sections/sections.slice";
 import input = Simulate.input;
+import { useDispatch, useSelector } from "react-redux";
 
 type SocialsView = {}
 type SocialItem = {
   network: string; enabled: boolean; prefix: string; value: string; isUrl: string
 }
-const SOCIAL_LINKS = [{
-  network: "Spotify", enabled: true, value: "", prefix: "", isUrl: true
-}, {
-  network: "Facebook",
-  enabled: true,
-  value: "ayomikel",
-  prefix: "facebook.com/",
-  isUrl: true
-}, {
-  network: "Twitter",
-  enabled: true,
-  value: "anagkazou",
-  prefix: "twitter.com/",
-  isUrl: false
-}, {
-  network: "Youtube", enabled: true, value: "dailywire", prefix: "", isUrl: true
-}, {
-  network: "Snapchat", enabled: true, value: "", prefix: "", isUrl: false
-}, {
-  network: "Linkedin", enabled: false, value: "lin", prefix: "", isUrl: true
-}, {
-  network: "Twitch", enabled: false, value: "", prefix: "", isUrl: false
-}, {
-  network: "Envelope",
-  enabled: false,
-  isUrl: true,
-  value: "thedavidomole",
-  prefix: ""
-}];
 
 
 const addSocialLink = () => {
@@ -65,9 +40,27 @@ type SocialView = {
   setPanelHeight: any
 }
 export const SocialsView: React.FC<SocialView> = ({ setPanelHeight }) => {
+  const sectionState = useSelector(selectSections);
+  const dispatch = useDispatch();
+  const socialLinksIndexInStore = useSelector(selectSocialLinks);
+  const socialLinksFromStore = sectionState.items[socialLinksIndexInStore];
 
   const [inputFieldInFocus, setInputFieldInFocus] = useState<number | null>(null);
-  const [socialLinks, setSocialLinks] = useState(SOCIAL_LINKS);
+  // @ts-ignore
+  const [socialLinks, setSocialLinks] = useState<Array<SocialItem>>(initialState);
+
+  function initialState() {
+    // @ts-ignore
+    //  console.log("USERSOCIALLINKSINSTORE::", socialLinksIndexInStore?.links);
+    console.log(socialLinksIndexInStore, "UINS");
+    if (socialLinksIndexInStore === -1) return DEFAULT_SOCIAL_LINKS; else {
+      console.log("PPPPP", socialLinksFromStore);
+      // @ts-ignore
+      return socialLinksFromStore.links;
+    }
+
+  }
+
   const isKeyboardOpen = useDetectKeyboardOpen();
 
   useEffect(() => {
@@ -75,11 +68,25 @@ export const SocialsView: React.FC<SocialView> = ({ setPanelHeight }) => {
       handleOnBlur(inputFieldInFocus);
       // @ts-ignore
       console.log("ooooo", inputRefs[inputFieldInFocus]);
-      inputRefs[inputFieldInFocus]?.current?.blur();
+      // @ts-ignore
+      inputRefs[inputFieldInFocus]?.blur();
+
+      inputRefs.forEach((inputRef: any) => inputRef?.blur());
     }
   }, [isKeyboardOpen]);
+  const [saved, setSavedState] = useState<boolean>(false);
 
-  const [saved, setSavedState] = useState(false);
+  useEffect(() => {
+    if (saved) {
+      console.log("touched");
+      dispatch(saveSocialLinks((socialLinks)));
+    }
+  }, [saved]);
+  useEffect(() => {
+    console.log("SOCIALLINKS,", socialLinks);
+  });
+
+
   const [temp, setTemp] = useState<string>(); //Todo: try to reduce state declarations... refs can hold some of these values
   const handleFocus = (i: number) => {
     setTemp(socialLinks[i].value);
@@ -118,20 +125,28 @@ export const SocialsView: React.FC<SocialView> = ({ setPanelHeight }) => {
 
   const saveUrl = (index: number, event: any) => {
     event.preventDefault();
+    //@ts-ignore
+    const value = inputRefs[inputFieldInFocus]?.value;
 
+    setSocialLinks(prev => prev.map((el, i) => i === index ? {
+      ...el, enabled: !!value.length
+    } : el));
     setSavedState(true);
+
     setInputFieldInFocus(null);
     setPanelHeight(70);
-    
+
   };
 
   const handleChange = (index: number, event: any) => {
-    const value = event.target.value;
+    const value = event.target.value.replace(/\s/g, "");
     // setSocialLinks(prevState => [...prevState, prevState[index].value = value]);
 
     setSocialLinks(prev => prev.map((el, i) => i === index ? {
       ...el, value: value
     } : el));
+
+
     setSavedState(false);
     //  setTempState()
     console.log("NEW STATE:::", socialLinks[index]);
@@ -139,20 +154,35 @@ export const SocialsView: React.FC<SocialView> = ({ setPanelHeight }) => {
   return (<div className="w-screen px-4 pt-4 socials-view fadeInLeft">
     <div className="pb-4 socials-view__links">
       {socialLinks.map((item, index) => (<form key={index}
-                                               className={`flex flex-row  
+                                               className={`flex flex-row mb-2 
                                                ${!item?.enabled && inputFieldInFocus !== index ? "absolute -left-full" : ""}
                                                `}>
-        <input key={index} id="Youtube"
-               onFocus={() => handleFocus(index)}
-               value={item?.value}
-          // onBlur={handleOnBlur}
-               onChange={(event) => handleChange(index, event)}
-               autoComplete={"off"}
-               name={item?.network}
-               autoFocus={true}
-               ref={setRef}
-               className={`mb-2  ${inputFieldInFocus !== index && inputFieldInFocus !== null ? "hidden" : ""}  p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
-               placeholder={item?.network} />
+        <div
+          className={`input-wrapper flex w-96 dark:bg-gray-700 rounded-lg border-b-neutral-300 ${inputFieldInFocus !== index && inputFieldInFocus !== null ? "hidden" : ""}`}>
+
+          <div className={`social flex items-center  `}>
+            <FontAwesomeIcon icon={getSocialIcon(item.network)} size={"1x"}
+                             color={"#fff"} />
+
+          </div>
+          <input key={index} id="Youtube"
+                 onFocus={() => handleFocus(index)}
+                 value={item?.value}
+            // onBlur={handleOnBlur}
+                 onChange={(event) => handleChange(index, event)}
+                 autoComplete={"off"}
+                 name={item?.network}
+                 autoFocus={true}
+                 ref={setRef}
+                 className={`  p-2.5 w-full text-sm text-gray-900 dark:text-gray-50  border-none bg-transparent`}
+                 placeholder={item?.network} />
+
+          <button
+            className={`text-sm text-gray-900 bg-transparent  border-none ${inputFieldInFocus == index ? "block" : "hidden"} `}
+          ><FontAwesomeIcon icon={faXmark} size={"2x"} color={"#fff"} />
+          </button>
+        </div>
+
         <button
           className={`text-sm text-gray-900 bg-transparent  border-none ${inputFieldInFocus == index ? "block" : "hidden"} `}
           onClick={(event) => saveUrl(index, event)}
