dfbe771be44525ffebc3f186694a3cf284ef1b2a
anagkazou <thedavidomole@gmail.com>
2022-11-12 19:48:24 +0100

    :bug: fix build for depoloyment

--- pages/editor/background-tab.tsx
+++ pages/editor/background-tab.tsx
@@ -4,9 +4,9 @@ export const BackgroundTabs = () => {
   return (
     <div className="background-tab">
       <div className="background-container">
-        {backgroundColors.map((el) => {
+        {backgroundColors.map((el, index) => {
           return (
-            <div className="background-option__wrapper">
+            <div key={index} className="background-option__wrapper">
               <div className="background-option" style={{ background: el.code.hex }}>
                 {" "}
               </div>
