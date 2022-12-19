f2368a9c38898ef03b2b0d493f88606cc0dd73c6
Anagkazou <thedavidomole@gmail.com>
2022-12-19 19:05:07 +0100

    💄implement styles Drawe UI

--- components/editor/drawers/style-drawer/index.tsx
+++ components/editor/drawers/style-drawer/index.tsx
@@ -1,12 +1,11 @@
 import { useEffect, useRef, useState } from "react";
-import { DrawerHeader } from "../../drawer-header";
 import { useDispatch } from "react-redux";
 import {
   setActiveDrawer
 } from "../../../../redux/features/ui-state/ui-state.slice";
 import { DrawerEnums } from "../../../../enums";
 import { BaseDrawer } from "../base-drawer";
-
+import { Icons } from "../../../../assets/icons";
 
 export const StyleDrawer = () => {
   const dispatch = useDispatch();
@@ -35,19 +34,23 @@ export const StyleDrawer = () => {
 
 
   return (<BaseDrawer drawerName={DrawerEnums.STYLE}>
-    <div >
-      <DrawerHeader />
+    <div>
 
-      <div className="flex h-[10rem] px-4 pt-4 pb-10">
-        <div className="style-option w-1/2 mr-2 p-2" onClick={() => {
+
+      <div
+        className="flex items-center justify-center h-[10rem] px-4 pt-4 pb-10">
+        <div className="style-option flex flex-col bg-gray-200 rounded-md justify-center place-items-center  w-1/2 mr-2 p-2" onClick={() => {
           dispatch(setActiveDrawer(DrawerEnums.BACKGROUND));
         }}>
-          BACKGROUND
+          <Icons.Template/>
+          <div className="text-sm text-center">Background</div>
         </div>
-        <div className="style-option w-1/2 p-2" onClick={() => {
+        <div className="style-option w-1/2 p-2 flex flex-col bg-gray-200 rounded-md justify-center place-items-center" onClick={() => {
           dispatch(setActiveDrawer(DrawerEnums.TEMPLATES));
         }}>
-          TEMPLATES
+          <Icons.Template/>
+          <div className="text-sm text-center" >Templates</div>
+
         </div>
       </div>
     </div>
